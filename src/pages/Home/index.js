241160f69f5e36652383eb2bbbe91d0f2dc7ce07
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2023-03-10 16:28:01 -0300

    created header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import useAuth from "../../hooks/useAuth";
 import * as C from "./styles";
+import Header from "../../components/Header";
 
 const Home = () => {
   const { signout } = useAuth();
@@ -10,6 +11,7 @@ const Home = () => {
 
   return (
     <C.Container>
+      <Header />
       <C.Title>Home</C.Title>
       <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
         Sair
